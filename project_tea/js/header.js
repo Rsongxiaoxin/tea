8493f989d54e7923fcedef4b6873978f4891729a
Rsongxiaoxin <44639845+Rsongxiaoxin@users.noreply.github.com>
2018-11-06 19:52:12 +0800

    Add files via upload

---
+++ project_tea/js/header.js
@@ -0,0 +1,16 @@
+function mouseOver()
+{
+    document.getElementById('d1').src ="img/header/weixinshangcheng.png"
+}
+function mouseOut()
+{
+    document.getElementById('d1').src ="img/header/zixunkefu.png"
+}
+function mou()
+{
+    document.getElementById('d2').src ="img/header/telphone.png"
+}
+function mo()
+{
+    document.getElementById('d2').src ="img/header/tel.png"
+}
