290b0a1b3b811fc7e44645218f9b1f2a5e86a20e
Rsongxiaoxin <44639845+Rsongxiaoxin@users.noreply.github.com>
2018-11-20 17:41:56 +0800

    Add files via upload

--- project_tea/js/nav_one.js
+++ project_tea/js/nav_one.js
@@ -1,19 +1,20 @@
 $(function(){
-    var $ul=$(".slider");//获得轮播图
-    var $ulIds=$(".bx-pager-item");//获得下边三个点
-    var LIEIFTH=1420,moved=0;//定义长度和移动距离
-    $ul.css("width",LIEIFTH*5);//更改轮播图宽度*5
-    $(".next").click(function () { //获得向右箭头点击事件
-        if(!$ul.is(":animated")){//判断如果轮播图不是动画效果
-            moved++;//点击一下移动+1
-            $ul.animate({//获得轮播动画
-                left:-moved*LIEIFTH//更改动画距离
-            },500,function(){//动画时间，
-                if(moved==3){
+    //轮播start
+    var $ul=$(".slider");
+    var $ulIds=$(".bx-pager-item");
+    var lieifth=1420,moved=0;
+    $ul.css("width",lieifth*5);
+    $(".next").click(function(){
+        if(!$ul.is(":animated")){
+            moved++;
+            $ul.animate({
+                left:-moved*lieifth
+            },500,function(){
+                if(moved == 3){
                     $ul.css("left",0);
                     moved=0;
                 }
-                $ulIds.children(":eq("+moved+")").addClass("active").siblings().removeClass("active");
+                $ulIds.children(":eq("+ moved +")").addClass("active").siblings().removeClass("active");
             });
         }
     });
@@ -21,57 +22,56 @@ $(function(){
         if(!$ul.is(":animated")) {
             if (moved == 0) {
                 moved = 3;
-                $ul.css("left", -moved * LIEIFTH);
+                $ul.css("left", -moved * lieifth);
             }
             moved--;
             $ul.animate({
-                left: -moved * LIEIFTH
+                left: -moved * lieifth
             }, 500, function () {
                 $ulIds.children(":eq(" + moved + ")").addClass("active").siblings().removeClass("active");
             });
         }
     });
-    $ulIds.on("click","a",function () {
-        if(!$ul.is(":animated")) {
-            var $a = $(this);
-            var i = $a.index();
-            moved = i;
+    $ulIds.on("click","a",function(){
+        if(!$ul.is(":animated")){
+            var $a=$(this);
+            var i=$a.index();
+            moved=i;
             $ul.animate({
-                left: -moved * LIEIFTH
-            }, 500, function () {
-                $ulIds.children(":eq(" + moved + ")").addClass("active").siblings().removeClass("active");
+                left:-moved*lieifth
+            },500,function(){
+                $ulIds.children(":eq("+ moved +")").addClass("active").siblings().removeClass("active");
             });
         }
     })
-    $(window).scroll(function() {
-        var scrollTop = $(window).scrollTop();
-        var $nav = $(".top>.header-nav");
-        if (scrollTop >= 78) {
-            $nav.addClass("suspension");
-        } else {
-            $nav.removeClass("suspension");
-        }
-    });
+//轮播end
+//手风琴
+
     $(".testing").on("click",".title",function () {
         var $content=$(this).next();
         if($content.is(".in"))
             $content.removeClass("in");
         else
             $content.addClass("in").siblings(".content").removeClass("in");
-    })
+    });
     $(".testing").on("click","li",function () {
         $(this).children(":last-child").addClass("determine").parent().siblings().children(":last-child").removeClass("determine");
-    }).on("mouseenter","li",function () {
-        var $p=$(this).children(":last-child")
-        if($p.hasClass("determine")==false){
-            $p.addClass("cho")
-        }
-    }).on("mouseleave","li",function () {
-        var $p=$(this).children(":last-child")
-        if($p.hasClass("cho")==true){
-            $p.removeClass("cho")
-        }
     })
-});
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
