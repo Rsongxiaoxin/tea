c1b265c5236ce7ff617fe6dc396644649214a44d
Rsongxiaoxin <44639845+Rsongxiaoxin@users.noreply.github.com>
2018-11-20 20:07:39 +0800

    Add files via upload

--- project_tea/js/tea_bottom_tan.js
+++ project_tea/js/tea_bottom_tan.js
@@ -1,9 +1,25 @@
+//底部弹窗
 window.onload=function(){
     msg.style.bottom="0px";
 }
 msg.children[1].onclick=function(){
-    msg.style.bottom="-200px";
+    msg.style.bottom="-150px";
     setTimeout(function(){
         msg.style.bottom="0px";
-    },6000)
+    },30000)
 }
+
+//侧边栏
+$(function() {
+    $(".Focus").mousemove(function () {
+        $(".nav-content").show();
+    }).mouseout(function () {
+        $(".nav-content").hide();
+    })
+    $(".phone").mousemove(function () {
+        $(".nav-content1").show();
+    }).mouseout(function () {
+        $(".nav-content1").hide();
+    })
+
+});
