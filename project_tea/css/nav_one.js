9da19c5e22c7e5d1ff1a9c773c3900f5c99eca85
Rsongxiaoxin <44639845+Rsongxiaoxin@users.noreply.github.com>
2018-11-30 19:26:22 +0800

    Add files via upload

---
+++ project_tea/css/nav_one.js
@@ -0,0 +1,92 @@
+$(function(){
+    //轮播start
+    var $ul=$(".slider");
+    var $ulIds=$(".bx-pager-item");
+    var lieifth=1420,moved=0;
+    $ul.css("width",lieifth*5);
+    $(".next").click(function(){
+        if(!$ul.is(":animated")){
+            moved++;
+            $ul.animate({
+                left:-moved*lieifth
+            },500,function(){
+                if(moved == 3){
+                    $ul.css("left",0);
+                    moved=0;
+                }
+                $ulIds.children(":eq("+ moved +")").addClass("active").siblings().removeClass("active");
+            });
+        }
+    });
+    $(".prev").click(function () {
+        if(!$ul.is(":animated")) {
+            if (moved == 0) {
+                moved = 3;
+                $ul.css("left", -moved * lieifth);
+            }
+            moved--;
+            $ul.animate({
+                left: -moved * lieifth
+            }, 500, function () {
+                $ulIds.children(":eq(" + moved + ")").addClass("active").siblings().removeClass("active");
+            });
+        }
+    });
+    $ulIds.on("click","a",function(){
+        if(!$ul.is(":animated")){
+            var $a=$(this);
+            var i=$a.index();
+            moved=i;
+            $ul.animate({
+                left:-moved*lieifth
+            },500,function(){
+                $ulIds.children(":eq("+ moved +")").addClass("active").siblings().removeClass("active");
+            });
+        }
+    })
+//轮播end
+//手风琴
+
+    $(".testing").on("click",".title",function () {
+        var $content=$(this).next();
+        if($content.is(".in"))
+            $content.removeClass("in");
+        else
+            $content.addClass("in").siblings(".content").removeClass("in");
+    });
+    $(".testing").on("click","li",function () {
+        $(this).children(":last-child").addClass("determine").parent().siblings().children(":last-child").removeClass("determine");
+    });
+
+
+    // 下拉菜单.ite>a:eq(0)
+    $(".ite").mouseenter(function () {//给a标签设置鼠标触发事件
+        //给observation删除原始属性，并添加新属性
+        $("#observation").removeClass("leave").addClass("observation")
+    }).mouseleave(function () {
+        //给observation失去焦点的时候
+        $("#observation").removeClass("observation").addClass("leave")
+    })
+    $("#observation").mouseenter(function () {
+        $("#observation").removeClass("leave").addClass("observation1")
+    }).mouseleave(function () {
+        $("#observation").removeClass("observation1").addClass("leave")
+    });
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
